f68d138f294774e8e35d632dc34b1e33e78e80cc
Pyatinskiy M.(zenn) <zenn1989@gmail.com>
2019-05-28 15:34:35 +0300

    add translation msg

--- src/I18n/Api/ru/Callback.php
+++ src/I18n/Api/ru/Callback.php
@@ -4,5 +4,6 @@ return [
     'New callback request for: %phone%' => 'Новый запрос обратной связи для: %phone%',
     'New callback request #%id%' => 'Новый запрос обратной связи №%id%',
     'New callback request from client: %phone% at %date%' => 'Новый запрос от клиента: %phone% от %date%',
-    'View request' => 'Посмотреть запрос'
+    'View request' => 'Посмотреть запрос',
+    'Request successful send. We we call you as soon as posible!' => 'Ваш запрос отправлен. Мы свяжемся с вами в ближайшее время!'
 ];
